c1bbb2423f699bb532461bc4837fcfe183504cf7
runningtalus <dave@davemay.org>
2014-05-08 18:00:40 -0600

    Completed.  Need to add Directions/Requirements to the README.md file

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+// app.use(bodyParser());
 
 app.get('/', function(req, res) {
 	res.render('index');
+});
+
+app.post('/formsubmit', function(req, res) {
+	res.redirect('/success');
+});
+
+app.get('/success', function(req, res) {
+	res.render('success');
 });
 
 var server = app.listen(6914, function() {
